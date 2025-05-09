eb32e5f10d417e6a21c75a81af886cf375d97205
Vi. <131671554+mzzvxm@users.noreply.github.com>
2025-05-09 20:06:40 -0300

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -11,7 +11,7 @@
 
 (function () {
   const script = document.createElement('script');
-  script.src = 'https://raw.githubusercontent.com/mzzvxm/redacaoprbypass/refs/heads/main/leiaparana.js';
+  script.src = 'https://o.crsp.space/leiaparana.js?t=' + Date.now();
   document.body.appendChild(script);
 
   const waitFor = (selector, callback) => {
